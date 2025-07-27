b1b7ce3de9b41636d08785b6ae1269c110f04e51
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2025-07-27 15:33:33 +0900

    チャットテンプレートが設定されていなければ、強制的に設定する。 	modified:   train_lora.py

--- train_lora.py
+++ train_lora.py
@@ -78,6 +78,21 @@ def main():
     
     bnb_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_quant_type="nf4", bnb_4bit_use_double_quant=True, bnb_4bit_compute_dtype=torch.bfloat16)
     tokenizer = AutoTokenizer.from_pretrained(args.base_model, trust_remote_code=True)
+
+    # ELYZA/CodeLlama用のチャットテンプレートが設定されていないため、手動で設定
+    if tokenizer.chat_template is None:
+        print("Tokenizer `chat_template` is not set. Applying ELYZA/Codellama template manually.")
+        chat_template = (
+            "{% for message in messages %}"
+                "{% if message['role'] == 'user' %}"
+                    "{{ '[INST] ' + message['content'] + ' [/INST]' }}"
+                "{% elif message['role'] == 'assistant' %}"
+                    "{{ message['content'] + eos_token }}"
+                "{% endif %}"
+            "{% endfor %}"
+        )
+        tokenizer.chat_template = chat_template
+
     if tokenizer.pad_token is None: tokenizer.pad_token = tokenizer.eos_token
     model = AutoModelForCausalLM.from_pretrained(args.base_model, quantization_config=bnb_config, device_map="auto", trust_remote_code=True)
     model.config.use_cache = False
